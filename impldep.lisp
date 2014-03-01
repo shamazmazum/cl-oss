6f22a16d34d0419935fbd49f9945aaefd32ff2d0
Vasily Postnicov <shamaz.mazum@gmail.com>
2014-03-01 22:35:01 +0400

    clisp support (no, not really)

--- impldep.lisp
+++ impldep.lisp
@@ -8,3 +8,13 @@
 (defun get-file-descriptor (stream direction)
   (declare (ignore direction))
   (sb-sys:fd-stream-fd stream))
+
+;; Broken for some reason
+#+clisp
+(defun get-file-descriptor (stream direction)
+  (declare (type (member :input :output) direction))
+  (multiple-value-bind (in-fd out-fd)
+      (ext:stream-handles stream)
+    (cond
+      ((eq :input direction) in-fd)
+      ((eq :output direction) out-fd))))
